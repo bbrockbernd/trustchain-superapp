0debfa967eb113aa59f3c48b65a8638aac70d33e
neal <n.sweijen@student.tudelft.nl>
2023-03-19 15:26:14 +0100

    Add resending of messages

--- detoks-engine/src/main/java/nl/tudelft/trustchain/detoks_engine/TransactionCommunity.kt
+++ detoks-engine/src/main/java/nl/tudelft/trustchain/detoks_engine/TransactionCommunity.kt
@@ -1,20 +1,23 @@
 package nl.tudelft.trustchain.detoks_engine
 
 import TestMessage
+import kotlinx.coroutines.delay
 import mu.KotlinLogging
 import nl.tudelft.ipv8.Community
 import nl.tudelft.ipv8.Peer
 import nl.tudelft.ipv8.messaging.Packet
+import kotlinx.coroutines.*
 
 private val logger = KotlinLogging.logger {}
 
 class TransactionCommunity: Community() {
     override val serviceId = "02315685d1932a144279f8248fc3db5899c5df8c"
     private var handler: (msg: String) -> Unit = logger::debug
-
+    private var tokensSend: ArrayList<String>
     init {
         messageHandlers[MessageId.Test_Message] = ::onMessage
         messageHandlers[MessageId.Ack] = ::onAck
+        tokensSend = ArrayList<String>()
     }
 
     fun setHandler(onMsg: (msg: String) -> Unit) {
@@ -25,22 +28,29 @@ class TransactionCommunity: Community() {
         val (peer, payload) = packet.getAuthPayload(TestMessage.Deserializer)
         logger.debug("Message received, ${peer.mid}", peer.mid + ": " + payload.message)
         handler("DemoCommunity, ${peer.mid} : ${payload.message}")
-        sendAck(peer)
+        sendAck(peer, payload.message)
     }
 
     private fun onAck(packet: Packet) {
-        val (peer, payload) = packet.getAuthPayload(TestMessage.Deserializer)
-        logger.debug("Ack, ${peer.mid}", peer.mid + ": " + payload.message)
+        val (peer, payload) = packet.getAuthPayload(AckMessage.Deserializer)
+        val removed = tokensSend.remove(payload.message);
+        logger.debug("Ack, ${peer.mid}, ${payload.message}, ${removed}", peer.mid + ": " + payload.message)
     }
 
     fun send(peer: Peer, token: String) {
         val packet = serializePacket(MessageId.Test_Message, TestMessage(token))
+        tokensSend.add(token);
         logger.debug("Send")
         send(peer.address, packet)
+        runBlocking {
+            launch {
+                resendMessage(peer, token)
+            }
+        }
     }
 
-    private fun sendAck(peer: Peer) {
-        val packet = serializePacket(MessageId.Ack, TestMessage("not received"))
+    private fun sendAck(peer: Peer, token: String) {
+        val packet = serializePacket(MessageId.Ack, AckMessage(token))
         logger.debug("Send ack")
         send(peer.address, packet)
     }
@@ -53,6 +63,16 @@ class TransactionCommunity: Community() {
             )
             send(peer.address, packet)
         }
+    }
+
+    private suspend fun resendMessage(peer: Peer , token: String) {
+        delay(10000)
+        if(!tokensSend.contains(token)) {
+            logger.debug("Dont message")
+            return
+        }
+        logger.debug("Resend message")
+        send(peer, token)
     }
 
     object MessageId {
